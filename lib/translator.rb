c80dbeba2c5a4c1e703a859167ef909729dc40cd
Qing Wang <chingwang.qw@gmail.com>
2019-09-28 20:53:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require "yaml"
 def load_library(emoticons)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons_list = YAML.load_file('./lib/emoticons.yml')
   get_meaning = emoticons.map do |key, value| return key
   get_emoticon = emoticons.map do |key, value| return value
   return emoticons
