5240d8b6b0970a7aac743f7ad8526350a966f234
Qing Wang <chingwang.qw@gmail.com>
2019-09-28 20:56:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,9 @@ require "yaml"
 def load_library(emoticons)
   emoticons_list = YAML.load_file('./lib/emoticons.yml')
   emoticons_hash = {}
+  emoticons_list.each do |emoti_pair|
+    
+  end 
 end
 
 def get_japanese_emoticon(emoticons)
